bf31977b7b34e5cadef4a08ff15d10ffd73983c1
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2016-09-17 19:15:34 +0800

    更改资源引用

--- theme/default/admin-updata.php
+++ theme/default/admin-updata.php
@@ -6,8 +6,8 @@
     <title>更新</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/plugins/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/plugins/ionicons/css/ionicons.min.css">
     <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/style.css">
     <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/dist/css/skins/_all-skins.min.css">
--- theme/default/login.php
+++ theme/default/login.php
@@ -7,8 +7,8 @@
   <title>登录 | <?php echo $siteinfo['name']; ?></title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/plugins/font-awesome/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/plugins/ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="<?php echo $siteinfo['theme']['url']; ?>/assets/plugins/iCheck/square/blue.css">
 
